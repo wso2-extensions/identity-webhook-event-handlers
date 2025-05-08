07088a099057d27b146c66aa2ec72878e5437685
Sadeesha Sathmina <sadeesha@wso2.com>
2025-05-08 11:02:11 +0530

    Formatting fixes

--- components/org.wso2.identity.webhook.caep.event.handler/src/main/java/org/wso2/identity/webhook/caep/event/handler/internal/component/CAEPEventHookHandlerDataHolder.java
+++ components/org.wso2.identity.webhook.caep.event.handler/src/main/java/org/wso2/identity/webhook/caep/event/handler/internal/component/CAEPEventHookHandlerDataHolder.java
@@ -18,8 +18,6 @@
 
 package org.wso2.identity.webhook.caep.event.handler.internal.component;
 
-import org.wso2.carbon.identity.organization.management.service.OrganizationManager;
-
 /**
  * A data holder class to keep the data of the event handler component.
  */
