87dd0963024756f3a226bed73eec38d974baa57a
Indeewai Wijesiri <indeewari@wso2.com>
2025-06-09 14:41:17 +0530

    Unit tests

--- components/org.wso2.identity.webhook.wso2.event.handler/src/main/java/org/wso2/identity/webhook/wso2/event/handler/internal/constant/Constants.java
+++ components/org.wso2.identity.webhook.wso2.event.handler/src/main/java/org/wso2/identity/webhook/wso2/event/handler/internal/constant/Constants.java
@@ -34,4 +34,7 @@ public class Constants {
     public static final String SCIM2_USERS_ENDPOINT = "/scim2/Users";
     public static final String FIRST_NAME_CLAIM_URI = "http://wso2.org/claims/givenname";
     public static final String LAST_NAME_CLAIM_URI = "http://wso2.org/claims/lastname";
+
+    public static final String SESSION_ID = "sessionId";
+    public static final String LOCATION_CLAIM_URI = "http://wso2.org/claims/location";
 }
